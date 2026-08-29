ccaa83e9b71c2dfc8aaea5061f92ec9d2d9e1dc0
xj-201 <78598943+xj-201@users.noreply.github.com>
2026-08-29 21:34:31 +0800

    Update build.gradle.kts

--- simple_live_app/android/build.gradle.kts
+++ simple_live_app/android/build.gradle.kts
@@ -19,6 +19,23 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// 👇 新增：自动给缺失 namespace 的第三方库插件补齐 namespace，兼容 AGP 8.0+
+subprojects {
+    afterEvaluate {
+        val androidExtension = extensions.findByName("android")
+        if (androidExtension != null) {
+            val getNamespace = androidExtension.javaClass.getMethod("getNamespace")
+            val currentNamespace = getNamespace.invoke(androidExtension)
+            if (currentNamespace == null) {
+                val setNamespace = androidExtension.javaClass.getMethod("setNamespace", String::class.java)
+                // 使用子项目的 group 或名称作为默认 namespace
+                val fallbackNamespace = "com.example.${project.name.replace("-", "_")}"
+                setNamespace.invoke(androidExtension, fallbackNamespace)
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
