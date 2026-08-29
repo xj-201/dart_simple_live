08d7bdf409fa68f467a07df0a52702bc5fd65f8b
xj-201 <78598943+xj-201@users.noreply.github.com>
2026-08-29 20:56:26 +0800

    Update settings.gradle.kts

--- simple_live_app/android/settings.gradle.kts
+++ simple_live_app/android/settings.gradle.kts
@@ -19,7 +19,9 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.9.1" apply false
+    // 将 com.android.application 调整为广泛兼容的 8.2.1 或 8.3.0 稳定版
+    id("com.android.application") version "8.2.1" apply false
+    // 保持与 Flutter 3.38 工具链一致的 Kotlin 插件版本
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
